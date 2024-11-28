0889f57714a7bb169db891e5e48d9b61391234d0
Elyar53 <elyarenver53@gmail.com>
2024-11-27 19:32:28 -0800

    Update Boat.java

--- src/main/java/com/dealership/server/Boat.java
+++ src/main/java/com/dealership/server/Boat.java
@@ -9,4 +9,14 @@ enum BoatBodystyle {
 }
 
 public class Boat extends Vehicle {
+
+  BoatBodystyle m_BodyStyle;
+
+  public Boat(String make, String model, String imageLink, int year, int price, boolean preOwned, CarBodyStyle bodyStyle ) {
+		
+		super(make, model, imageLink, year, price, preOwned);
+
+    m_BodyStyle = bodyStyle;
+
+  }
   
