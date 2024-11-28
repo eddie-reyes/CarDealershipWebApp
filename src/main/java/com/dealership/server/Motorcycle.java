9b7bfe5413b82cc265123cb8dbfe9e323b4bbcd4
Elyar53 <elyarenver53@gmail.com>
2024-11-27 19:14:38 -0800

    Update Motorcycle.java

--- src/main/java/com/dealership/server/Motorcycle.java
+++ src/main/java/com/dealership/server/Motorcycle.java
@@ -9,5 +9,19 @@ enum BikeBodyStyle {
 
 }
 
-public class 
+public class Motorcycle extends Vehicle {
+
+  BikeBodyStyle m_BodyStyle;
+
+  public Motorcycle(String make, String model, String imageLink, int year, int price, double milesPerGallon, boolean preOwned, BikeBodyStyle bodyStyle ) {
+		
+		super(make, model, imageLink, year, price, milesPerGallon, preOwned);
+
+    m_BodyStyle = bodyStyle;
+		
+
+  
+
+  
+  
 
