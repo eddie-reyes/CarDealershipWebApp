d329df04648e522c9522c01e1417422e550efc11
Ailewaer Ailiyaer <elyarenver53@gmail.com>
2024-11-29 15:56:27 -0800

    Motorcycle.java

--- src/main/java/com/dealership/server/Motorcycle.java
+++ src/main/java/com/dealership/server/Motorcycle.java
@@ -14,11 +14,11 @@ public class Motorcycle extends Vehicle {
 
   BikeBodyStyle m_BodyStyle;
 
-  public Motorcycle(String make, String model, String imageLink, int year, int price, double milesPerGallon, boolean preOwned, BikeBodyStyle bodyStyle ) {
+  public Motorcycle(String make, String model, String imageLink, int year, int price, double milesPerGallon, boolean preOwned, DriveTrainType driveTrain, BikeBodyStyle bodyStyle) {
 		
 		super(make, model, imageLink, year, price, milesPerGallon, preOwned);
 
-    m_BodyStyle = bodyStyle;
+    		m_BodyStyle = bodyStyle;
 
   }
 }
