32f91ff5c651bedfa8ca798cc1050c1bde279571
eddie-reyes <eddie.kingsreyes@gmail.com>
2024-11-30 08:59:11 -0800

    fixed typos

--- src/main/java/com/dealership/server/Boat.java
+++ src/main/java/com/dealership/server/Boat.java
@@ -1,6 +1,6 @@
 package com.dealership.server;
 
-enum BoatBodystyle {
+enum BoatBodyStyle {
 
   SPEED,
   YACHT,
@@ -12,12 +12,17 @@ public class Boat extends Vehicle {
 
   private BoatBodystyle bodyStyle;
 
-  public Boat(String make, String model, String imageLink, int year, int price, boolean preOwned, CarBodyStyle bodyStyle ) {
+  public Boat(String make, String model, String imageLink, int year, int price, boolean preOwned, BoatBodyStyle bodyStyle ) {
 		
 		super(make, model, imageLink, year, price, preOwned);
 
     this.bodyStyle = bodyStyle;
 
+  }
+  
+  public String getSpecialFeatures() {
+	  
+	  return "";
   }
 
 }
