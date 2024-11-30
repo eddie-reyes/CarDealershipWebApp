18897366191425a2956b32cbc11e4783f42d3861
Ailewaer Ailiyaer <elyarenver53@gmail.com>
2024-11-29 16:20:40 -0800

    Boat.java (Debugged + Special Features)

--- src/main/java/com/dealership/server/Boat.java
+++ src/main/java/com/dealership/server/Boat.java
@@ -23,4 +23,34 @@ public class Boat extends Vehicle {
 
   }
 
+    @Override
+    public String getSpecialFeatures() {
+        String retval = "Body: ";
+
+        switch (m_BodyStyle) {
+            case SPEED:
+                retval += "Speed Boat";
+                break;
+            case YACHT:
+                retval += "Yacht";
+                break;
+            case SAILBOAT:
+                retval += "Sailboat";
+                break;
+            case CRUISER:
+                retval += "Cruiser";
+                break;
+            case FLYBRIDGE:
+                retval += "Flybridge";
+                break;
+            case BOWRIDER:
+                retval += "Bowrider";
+                break;
+            default:
+                retval += "Unknown";
+        }
+
+        return retval;
+    }
+	
 }
